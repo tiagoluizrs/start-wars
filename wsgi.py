2ea5b368d751de3115a78bc3fa205e861d1f248c
Tiago Luiz <tiagoluizribeirodasilva@gmail.com>
2019-07-09 18:12:06 -0300

    Procfile

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import sys
+from app import create_app
+from config import app_config, app_active
+
+config = app_config[app_active]
+config.APP = create_app(app_active)
+app = config.APP
+
+if __name__ == '__main__':
+    app.run(host=config.IP_HOST, port=config.PORT_HOST)
+    reload(sys)
